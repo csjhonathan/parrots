5026ea30e2583c778f18f4bca485f739dd7c9d38
csjhonathan <jhonathancarv.s@gmail.com>
2023-01-12 14:25:51 -0300

    implementação: distribuição das cartas, imagens back/front-faces e clicks nas cartas

--- script/script.js
+++ script/script.js
@@ -1,6 +1,12 @@
 //criar as cartas
+const arr = [];
 let cardsQttDistribution = 0;
 let board = document.querySelector('.board');
+
+let cardUm = '';
+let cardDois = '';
+
+let moves = 0;
 
 
 const createElement = (tag, className) => {
@@ -9,19 +15,25 @@ const createElement = (tag, className) => {
     return element
 }
 
-const cardCreator = () => {
+const cardCreator = (numero) => {
 
     const card = createElement('div', 'card');
-    const front = createElement('div', 'front-face face');
-    const back = createElement('div', 'back-face face');
+    const front = createElement('div', 'front face');
+    const back = createElement('div', 'back face');
+    const imgBackFace = createElement('img', 'backFaceImg');
+    const imgFrontFace = createElement('img', 'frontFaceImg')
 
-    front.innerHTML = "frente"
-    back.innerHTML = "verso"
+    imgBackFace.setAttribute('src', './assets/back.png')
+    imgFrontFace.setAttribute('src', `./assets/FrontFace${numero}.gif`)
+
+    front.appendChild(imgFrontFace)
+    back.appendChild(imgBackFace)
 
     card.appendChild(front);
     card.appendChild(back);
 
-    card.setAttribute("onclick", "selectCard(this)")
+    card.setAttribute("onclick", "flipCard(this)")
+    card.setAttribute('data-cardId', numero)
 
     board.appendChild(card);
     return card
@@ -29,15 +41,20 @@ const cardCreator = () => {
 }
 
 function boardPreparation() {
+    
     //quantidade de cartas que o usuário quer
     cardsQttDistribution = prompt("COM QUANTAS CARTAS DESEJA JOGAR? ESCOLHA ENTRE: 4, 6, 8, 10, 12 OU 14!");
+    for(let i = 0; i < (cardsQttDistribution/2); i++){
+        arr.push(i)
+    }
 
+    const shuffledArry = ([...arr, ...arr]).sort(()=> Math.random () - 0.5)
     //verificar se o número digitado é [4,14]%2===0
     if (cardsQttDistribution >= 4 && cardsQttDistribution <= 14 && cardsQttDistribution % 2 === 0) {
         //se for, começa o jogo
-        for(let i = 0; i< cardsQttDistribution;i++){
+        for (let i = 0; i < cardsQttDistribution; i++) {
             //crie as cartas
-            cardCreator(cardsQttDistribution);
+            cardCreator(shuffledArry[i]);
         }
         return;
     } else {
@@ -46,3 +63,44 @@ function boardPreparation() {
     }
 }
 boardPreparation();
+
+function verifyCards (){
+    const cardOne = cardUm.getAttribute('data-cardId');
+    const cardTwo = cardDois.getAttribute('data-cardId');
+    
+    if (cardOne === cardTwo){
+        cardUm = '';
+        cardDois = '';
+    } else {
+        setTimeout(() => {
+            cardUm.classList.remove('flip')
+            cardDois.classList.remove('flip')
+
+            cardUm = '';
+            cardDois = '';
+
+        }, 1200);
+    }
+}
+
+function flipCard(cards) {
+    
+    if(cards.classList.contains('flip')){
+        return
+    }
+
+    if(cardUm === ''){
+        moves += 1;
+        cards.classList.add('flip');
+        cardUm = cards;
+    } else if (cardDois === ''){
+        moves += 1;
+        cards.classList.add('flip');
+        cardDois = cards;
+
+        verifyCards(cards)
+    }
+    
+    console.log(moves)
+}
+

74d5ad60829ddf13c725b93f6925b70acd302dcf
csjhonathan <jhonathancarv.s@gmail.com>
2023-01-13 00:06:17 -0300

    organizando o script.js, separando algumas funções e corrigido bug que fazia com que o jogo iniciasse sempre com as mesmas cartas

--- script/script.js
+++ script/script.js
@@ -1,95 +1,119 @@
-const arr = [];
+const arr = [0, 1, 2, 3, 4, 5, 6].sort(() => Math.random() - 0.5); // <-- primeira randomizção
+//armazeno a quantidade de cartas que o usuário digitou;
 let cardsQttDistribution = 0;
+//armazeno aqui o "tabuleiro";
 let board = document.querySelector('.board');
 
+//armazeno a primeira carta clicada;
 let cardUm = '';
+//armazeno a segunda carta clicada;
 let cardDois = '';
 
+//contador de movimentos;
 let moves = 0;
 
+//pegando o cronometro no html;
 let cron = document.querySelector('.timer');
-let seconds = 0;
 
-//criar as cartas
+// aqui nós montamos os elementos que compõem as cartas
 const createElement = (tag, className) => {
     const element = document.createElement(tag);
     element.className = className;
     return element
 }
 
-const cardCreator = (numero) => {
+//aqui nós montamos ar cartas
+const cardCreator = (cardId) => {
 
-    const card = createElement('div', 'card');
-    const front = createElement('div', 'front face');
-    const back = createElement('div', 'back face');
-    const imgBackFace = createElement('img', 'backFaceImg');
-    const imgFrontFace = createElement('img', 'frontFaceImg')
+    //criando os elementos
+    const card = createElement('div', 'card');//<-- a carta em cim
+    const front = createElement('div', 'front face');//<-- div que recebe a face com o gif
+    const back = createElement('div', 'back face');//<-- div que recebe a face com o parrot estático
+    const imgBackFace = createElement('img', 'backFaceImg');//<-- tag img do parrot estático em si
+    const imgFrontFace = createElement('img', 'frontFaceImg')//<-- tag img do parrot gif em si
 
     //inserindo as imagens
-    imgBackFace.setAttribute('src', './assets/back.png');
-    imgFrontFace.setAttribute('src', `./assets/FrontFace${numero}.gif`);
+    imgBackFace.setAttribute('src', './assets/back.png'); //<-- adicionando os atributos da tag img
+    imgFrontFace.setAttribute('src', `./assets/FrontFace${cardId}.gif`);//<-- adicionando os atributos da tag img
 
     //inserindo os data-tests
-    imgBackFace.setAttribute('data-test', 'face-down-image');
-    imgFrontFace.setAttribute('data-test', `face-up-image`);
-    card.setAttribute('data-test', 'card');
+    imgBackFace.setAttribute('data-test', 'face-down-image');//<-- adicionando os atributos data-test para a avaliação do projeto
+    imgFrontFace.setAttribute('data-test', `face-up-image`);//<-- adicionando os atributos data-test para a avaliação do projeto
+    card.setAttribute('data-test', 'card');//<-- adicionando os atributos data-test para a avaliação do projeto
 
     //inserindo as imgs dentro das divs
-    front.appendChild(imgFrontFace);
-    back.appendChild(imgBackFace);
+    front.appendChild(imgFrontFace);//<-- adicionando as tags img dentro das divs
+    back.appendChild(imgBackFace);//<-- adicionando as tags img dentro das divs
 
     //inserindo as faces das cartas
-    card.appendChild(front);
-    card.appendChild(back);
+    card.appendChild(front);//<-- adicionando as divs dentro da div.card
+    card.appendChild(back);//<-- adicionando as divs dentro da div.card
 
-    card.setAttribute("onclick", "flipCard(this)");
-    card.setAttribute('data-cardId', numero);
-    
-    
-    board.appendChild(card);
+    card.setAttribute("onclick", "flipCard(this)");//<-- colocando atributo onclick na div.card
+    card.setAttribute('data-cardId', cardId);//<-- colocando atributo data-cardID para verificação posterior e o ID passado pelo criador de IDs
+
+    //insere a carta no board
+    board.appendChild(card);//<--inserindo no Board a carte, finalmente, criada
     return card
 
 }
 
-function startCron(){
-    console.log("estou aqui")
-    this.cronometer = setInterval(()=> {
-        const currentTime = +cron.innerHTML;
-        cron.innerHTML =  currentTime + 1;
-    }, 1000)
-}
-function boardPreparation() {
-    
-    //quantidade de cartas que o usuário quer
-    cardsQttDistribution = prompt("COM QUANTAS CARTAS DESEJA JOGAR? ESCOLHA ENTRE: 4, 6, 8, 10, 12 OU 14!");
-    for(let i = 0; i < (cardsQttDistribution/2); i++){
-        arr.push(i)
+//randomizador da ordem das cartas
+const cardsRandomizer = function (cardsQtt) {
+    //cardsQtt é um valor recebido pela F() boardPreparation, que recebeu de gamePreparation
+    let randomArr = []; // <-- array vazio que recebe o ID das cartas na ordem que foi recebida no arr;
+    for (let i = 0; i < (cardsQtt / 2); i++) {
+        randomArr.push(arr[i]) //<-- envia para o randomArr os IDs na metade da quantidade que o user pedir
     }
 
-    const shuffledArry = ([...arr, ...arr]).sort(()=> Math.random () - 0.5)
+    const shuffledArray = ([...randomArr, ...randomArr]).sort(() => Math.random() - 0.5);//<-- segunda randomização, recebe o randomArr 2x e ramdomiza de novo
+    return shuffledArray //<--retorna os ids embaralhados
+}
+
+function startCron() {
+
+    this.cronometer = setInterval(() => {
+        const currentTime = +cron.innerHTML;
+        cron.innerHTML = currentTime + 1;
+    }, 1000)
+
+}
+const sendCardsId = function (cardsQttShuffled) { //<-- recebe os ids já embaralhados 2x numa array que tem o tamanho de cartas que o user pediu
+    for (let i = 0; i < cardsQttShuffled.length; i++) {//<-- percorre a array e vai enviando os IDs para a F() criadora de cartas
+        cardCreator(shuffledArray[i]); //<-- recebe um ID e envia para o criador de cartas
+    }
+}
+
+//quantidade de cartas que o usuário quer
+function gamePreparation() {
+    cardsQttDistribution = prompt("COM QUANTAS CARTAS DESEJA JOGAR? ESCOLHA ENTRE: 4, 6, 8, 10, 12 OU 14!"); //<-- perguntando com quantas cartas o user quer jogar
+    boardPreparation(cardsQttDistribution); //<--enviando a informação para a boardPreparation() para a preparação do jogo
+}
+
+gamePreparation()
+
+
+function boardPreparation(cardsQtt) {//<--recebe a informação do gamePreparation sobre a quantidade de cartas
+
     //verificar se o número digitado é [4,14]%2===0
-    if (cardsQttDistribution >= 4 && cardsQttDistribution <= 14 && cardsQttDistribution % 2 === 0) {
-        //se for, começa o jogo e o timer
-        for (let i = 0; i < cardsQttDistribution; i++) {
-            //crie as cartas
-            cardCreator(shuffledArry[i]);
-        }
-        startCron()
+    if (cardsQtt >= 4 && cardsQtt <= 14 && cardsQtt % 2 === 0) {
+        //se for, passe para o criador de IDs e inicie o cronometro
+        shuffledArray = cardsRandomizer(cardsQtt);//<-- o criador de IDs recebeu a quantidade de cartas, e criou e devolveu os IDs já embaralhados numa array;
+        sendCardsId(shuffledArray)//<-- uma vez que tenha recebido os ids, enviamos para a função que vai distribuir para o criador de cartas
+        startCron()//<-- inicia o cronômetro
         return;
     } else {
         //se não for, pergunte novamente
-        boardPreparation();
+        gamePreparation();
     }
-
-    
 }
-boardPreparation();
 
-function verifyCards (){
+
+function verifyCards() {
     const cardOne = cardUm.getAttribute('data-cardId');
     const cardTwo = cardDois.getAttribute('data-cardId');
-    
-    if (cardOne === cardTwo){
+
+    if (cardOne === cardTwo) {
         cardUm = '';
         cardDois = '';
     } else {
@@ -105,16 +129,16 @@ function verifyCards (){
 }
 
 function flipCard(cards) {
-    
-    if(cards.classList.contains('flip')){
+
+    if (cards.classList.contains('flip')) {
         return
     }
 
-    if(cardUm === ''){
+    if (cardUm === '') {
         moves += 1;
         cards.classList.add('flip');
         cardUm = cards;
-    } else if (cardDois === ''){
+    } else if (cardDois === '') {
         moves += 1;
         cards.classList.add('flip');
         cardDois = cards;
@@ -126,20 +150,20 @@ function flipCard(cards) {
 function restartGame() {
     let response = prompt('Deseja continuar? DIGITE: "sim" ou "não"');
 
-    if(response!=='sim' && response!=='não'){
+    if (response !== 'sim' && response !== 'não') {
         restartGame()
-    }else if(response==='sim'){
+    } else if (response === 'sim') {
         location.reload();
-    }else{
+    } else {
         return
     }
 }
 
-function endGame(moves){
-    if(Number(cardsQttDistribution)===document.querySelectorAll('.flip').length){
+function endGame(moves) {
+    if (Number(cardsQttDistribution) === document.querySelectorAll('.flip').length) {
         clearInterval(this.cronometer);
         setTimeout(() => {
-            alert (`Você ganhou em ${moves} jogadas! A duração do jogo foi de ${cron.innerHTML} segundos!`);
+            alert(`Você ganhou em ${moves} jogadas! A duração do jogo foi de ${cron.innerHTML} segundos!`);
             restartGame()
         }, 500)
 
